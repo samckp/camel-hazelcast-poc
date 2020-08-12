9b294d0cb9cc8723c194482973992e239758da85
samckp <sam.mca.liet@gmail.com>
2020-08-12 09:40:09 +0530

    updated

--- src/main/java/info/camel/hazelcast/MapRoute.java
+++ src/main/java/info/camel/hazelcast/MapRoute.java
@@ -1,2 +1,43 @@
-package info.camel.hazelcast;public class MapRoute {
+package info.camel.hazelcast;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.SSLConfig;
+import com.hazelcast.core.HazelcastInstance;
+
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.hazelcast.HazelcastConstants;
+import org.apache.camel.component.hazelcast.HazelcastOperation;
+import org.apache.camel.component.hazelcast.topic.HazelcastTopicComponent;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapRoute extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // setup hazelcast
+        ClientConfig config = new ClientConfig();
+//        config.getNetworkConfig().addAddress("hazelcast");
+        config.getNetworkConfig().addAddress("192.168.43.93:5701");
+        config.getNetworkConfig().setSSLConfig(new SSLConfig().setEnabled(false));
+        HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
+
+        // setup camel hazelcast
+        HazelcastTopicComponent hazelcast = new HazelcastTopicComponent();
+        hazelcast.setHazelcastInstance(instance);
+        getContext().addComponent("hazelcast-topic", hazelcast);
+
+        from("direct:put")
+                .setHeader(HazelcastConstants.OPERATION, constant(HazelcastOperation.PUT))
+                .toF("hazelcast-%sfoo", HazelcastConstants.MAP_PREFIX);
+
+        /*from("")
+                .routeId("hazelcastRoute")
+                .log(LoggingLevel.INFO, "${body}")
+                .to("")
+                ;*/
+    }
 }
